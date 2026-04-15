09a053306b0fdc26da0585591fb3eb393c41a295
Jpcd <145725003+JhampierCD@users.noreply.github.com>
2026-04-14 23:22:47 -0500

    Commit Inicial - Backend ( InkaMetrics_BackEnds

---
+++ InkaMetrics_BackEnd/src/main/java/pe/edu/upc/inkametrics_backend/InkaMetricsBackEndApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.inkametrics_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkaMetricsBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InkaMetricsBackEndApplication.class, args);
+    }
+
+}
---
+++ InkaMetrics_BackEnd/src/test/java/pe/edu/upc/inkametrics_backend/InkaMetricsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.inkametrics_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkaMetricsBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
