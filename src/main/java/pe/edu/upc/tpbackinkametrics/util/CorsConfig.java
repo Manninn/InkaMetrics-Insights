cef424ae723d51dc159721612252f9fefac50ea4
Félix Luis López <fl.lopez.321@gmail.com>
2026-06-21 04:17:21 -0500

    fix: agregar ruta  frontend de produccion al CORS

--- src/main/java/pe/edu/upc/tpbackinkametrics/util/CorsConfig.java
+++ src/main/java/pe/edu/upc/tpbackinkametrics/util/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200")); // origen permitido: frontend Angular
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://frontend-inkametricks.onrender.com")); // origen permitido: frontend Angular local y producción
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // métodos HTTP permitidos (OPTIONS es requerido para preflight)
         config.setAllowedHeaders(List.of("*")); // acepta cualquier cabecera (Authorization, Content-Type, etc.)
 
